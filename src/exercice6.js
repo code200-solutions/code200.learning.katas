105ec53cdfb38183b60f2b637fb9b9fa666893ca
Boris Gbahoue <bgbahoue@gmail.com>
2024-08-14 15:36:23 +1100

    Update: v2

--- src/exercice6.js
+++ src/exercice6.js
@@ -1,4 +1,4 @@
-function sum6(a) {
+function sum6a(a) {
   let sum = 0;
   a.forEach((e) => {
     sum = sum + e;
@@ -6,5 +6,18 @@ function sum6(a) {
   return sum;
 }
 
-const total = sum6([1, 2, 3, 4, 5]);
-console.log(`total = ${total}`);
+function sum6b(a) {}
+
+function sum6c(a) {}
+
+const totalA = sum6a([1, 2, 3, 4, 5]);
+const totalB = sum6b([1, 2, 3, 4, 5]);
+const totalC = sum6c([1, 2, 3, 4, 5]);
+
+if (totalA === totalB && totalB === totalC) {
+  console.log('totalA, totalB and totalC are equals');
+} else {
+  console.log(
+    `There is a bug somewhere [totalA=${totalA}, totalB=${totalB}, totalC=${totalC}]`
+  );
+}
